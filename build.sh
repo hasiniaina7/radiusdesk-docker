c6e4c9f5485a28eb5af3d05aa7548851bd57891d
keegan <keeganthomaswhite@gmail.com>
2021-11-30 16:48:43 +0200

    Created docker image

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!bin/sh
-
-docker build ./ -t enock/radiusdesk3-php7-ubuntu16-supervisord-v3
-docker run --restart unless-stopped --net macvlan0 --ip 10.2.1.18  --name  radiusdeskv3.0 -it enock/radiusdesk3-php7-ubuntu16-supervisord-v3
